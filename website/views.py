fc6aa3195b7672017254bae5cba31c1bbe7abab6
FRotatori99 <rotatori.federico99@gmail.com>
2022-02-12 10:02:40 +0100

    Home page

--- website/views.py
+++ website/views.py
@@ -1,8 +1,12 @@
 from flask import Blueprint, render_template
+from datetime import date
 
 views = Blueprint("views", __name__)
 
 @views.route("/")
 @views.route("/home")
 def home():
-    return render_template("home.html")
+    birthdate = date(day=28, month=3, year=1999)
+    today = date.today()
+    age = today.year - birthdate.year - ((today.month, today.day) < (birthdate.month, birthdate.day))
+    return render_template("home.html", age = age)
