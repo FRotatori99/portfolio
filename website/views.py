3233a7f554b06e97b7cca3a050fc5dfc20f50ad6
FRotatori99 <rotatori.federico99@gmail.com>
2022-02-13 13:27:09 +0100

    about me

--- website/views.py
+++ website/views.py
@@ -10,3 +10,15 @@ def home():
     today = date.today()
     age = today.year - birthdate.year - ((today.month, today.day) < (birthdate.month, birthdate.day))
     return render_template("home.html", age = age)
+
+@views.route("/su-di-me/chi-sono")
+def about_me():
+    return render_template("su-di-me/about_me.html")
+
+@views.route("/su-di-me/perchè-questo-sito")
+def why_this_site():
+    return render_template("")
+
+@views.route("/su-di-me/citazioni-preferite")
+def favourite_quotes():
+    return render_template("")
