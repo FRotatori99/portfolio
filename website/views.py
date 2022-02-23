e3ccc0b149c832deb4208108a7ef7df0a9a39b99
FRotatori99 <rotatori.federico99@gmail.com>
2022-02-23 23:29:42 +0100

    contacts

--- website/views.py
+++ website/views.py
@@ -11,15 +11,27 @@ def home():
     age = today.year - birthdate.year - ((today.month, today.day) < (birthdate.month, birthdate.day))
     return render_template("home.html", age = age)
 
+# About me
 @views.route("/su-di-me/chi-sono")
 def about_me():
-    return render_template("su-di-me/about_me.html")
+    return render_template("about_me/about_me.html")
 
 @views.route("/su-di-me/perchè-questo-sito")
 def why_this_site():
-    return render_template("su-di-me/why_this_site.html")
+    return render_template("about_me/why_this_site.html")
 
 @views.route("/su-di-me/citazioni-preferite")
 def quotes():
-    return render_template("")
+    return render_template("about_me/favourite_quotes.html")
 
+
+# Portfolio
+
+# Referral
+
+# Blog
+
+# Contacts
+@views.route("/contacts")
+def contacts():
+    return render_template("contacts.html")
